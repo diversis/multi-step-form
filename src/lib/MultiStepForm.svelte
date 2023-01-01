57dde1749724d3a5151208212dd30bee4e32803a
Noxyangel <forgotrrr@gmail.com>
2023-01-01 17:26:58 +0300

    waves animation 1

--- src/lib/MultiStepForm.svelte
+++ src/lib/MultiStepForm.svelte
@@ -6,26 +6,32 @@
 	class="rounded-lg mx-auto container grid grid-cols-3 h-[80%] shadow-around-lg shadow-light-blue"
 >
 	<div class="relative rounded-lg p-4">
-		<article
-			class="relative rounded-lg  w-full h-full bg-[url('/waves.svg')] bg-purplish-blue bg-bottom"
-		>
-			<ul
-				class="flex flex-col justify-start pt-8 text-xl text-left w-[14ch] mx-auto gap-8 uppercase "
+		<div class="relative rounded-lg w-full h-full  bg-purplish-blue overflow-hidden">
+			<div
+				id="bg-waves"
+				class="absolute rounded-lg w-[200rem] h-full bg-[url('/waves-2.svg')] bg-transparent"
+			/>
+			<article
+				class="relative rounded-lg  w-full h-full bg-[url('/polygon.svg')] bg-transparent bg-bottom"
 			>
-				<li id="step-1" class="flex flex-col font-extralight text-cool-gray">
-					Step 1<span class="font-bold text-white tracking-wider">Your info</span>
-				</li>
-				<li id="step-2" class="flex flex-col font-extralight text-cool-gray">
-					Step 2<span class="font-bold text-white tracking-wider">Select plan</span>
-				</li>
-				<li id="step-3" class="flex flex-col font-extralight text-cool-gray">
-					Step 3<span class="font-bold text-white tracking-wider">Add-ons</span>
-				</li>
-				<l id="step-4" class="flex flex-col font-extralight text-cool-gray">
-					Step 4<span class="font-bold text-white tracking-wider">Summary</span>
-				</l>
-			</ul>
-		</article>
+				<ul
+					class="flex flex-col justify-start pt-8 text-xl text-left w-[14ch] mx-auto gap-8 uppercase "
+				>
+					<li id="step-1" class="flex flex-col font-extralight text-cool-gray">
+						Step 1<span class="font-bold text-white tracking-wider">Your info</span>
+					</li>
+					<li id="step-2" class="flex flex-col font-extralight text-cool-gray">
+						Step 2<span class="font-bold text-white tracking-wider">Select plan</span>
+					</li>
+					<li id="step-3" class="flex flex-col font-extralight text-cool-gray">
+						Step 3<span class="font-bold text-white tracking-wider">Add-ons</span>
+					</li>
+					<l id="step-4" class="flex flex-col font-extralight text-cool-gray">
+						Step 4<span class="font-bold text-white tracking-wider">Summary</span>
+					</l>
+				</ul>
+			</article>
+		</div>
 	</div>
 	<div class="rounded-lg col-span-2">
 		<h1>Personal info</h1>
@@ -35,6 +41,24 @@
 </div>
 
 <style>
+	#bg-waves {
+		outline: 1px solid green;
+		background-repeat: repeat-x;
+		background-position: bottom;
+		background-size: auto;
+		animation-name: waves;
+		animation-duration: 10s;
+		animation-iteration-count: infinite;
+		animation-timing-function: linear;
+	}
+	@keyframes waves {
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(calc(24.375rem - 100%));
+		}
+	}
 	/* #form > * {
 		outline: 1px solid red;
 	}
